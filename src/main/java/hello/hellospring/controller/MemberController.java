51afda66d63d04953a98b56dd29c008e406fc863
kanghanju <hanju1017@gmail.com>
2023-08-26 12:51:21 +0900

    feat:스프링 웹 개발 기초(웹 MVC 개발)

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,29 @@ public class MemberController {
     @Autowired //생성자에 Autowired가 있으면 memberService를 spring이 container에 있는 memberService와 연결시킨다
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+
+    @GetMapping("members/new")
+    public String createdForm(){
+        return "members/createMemberForm";
+    }
+
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String List(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);//key는 members,members안에는 List로 모든 회원을 담아냈다.
+        return "members/memberList";
     }
 }
